e68d89015f42eec7c01d6a1e8dc57fc5cdd4ec3a
Billy Maguire <wmaguire@nexient.com>
2021-04-14 14:28:56 -0400

    Factor out the layout/styling of GFAlert containerView into its own class

--- GHFollowers/Custom_Views/ViewControllers/GFAlertViewController.swift
+++ GHFollowers/Custom_Views/ViewControllers/GFAlertViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class GFAlertViewController: UIViewController {
 
-    let containerView = UIView()
+    let containerView = GFAlertContainerView()
     let titleLabel = GFTitleLabel(textAlignment: .center, fontSize: 20)
     let messageLabel = GFBodyLabel(textAlignment: .center)
     let actionButton = GFButton(backgroundColor: .systemRed, title: "Ok")
@@ -34,7 +34,7 @@ class GFAlertViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Alpha is same as Opacity, Zeroes for Black
-        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.75)
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.75)
         configureContainerView()
         configureTitleLabel()
         configureActionButton()
@@ -43,11 +43,6 @@ class GFAlertViewController: UIViewController {
     
     func configureContainerView() {
         view.addSubview(containerView)
-        containerView.backgroundColor = .systemBackground
-        containerView.layer.cornerRadius = 16
-        containerView.layer.borderWidth = 2
-        containerView.layer.borderColor = UIColor.white.cgColor
-        containerView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             containerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
